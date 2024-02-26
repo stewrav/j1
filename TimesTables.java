a7e8f28ab65274dd003f8858e380292d1471febb
Stewart Ravenhall <stewrav@gmail.com>
2024-02-26 20:37:27 +0000

    Finished TimesTables

--- TimesTables.java
+++ TimesTables.java
@@ -7,7 +7,8 @@ public class TimesTables {
 
         //System.out.println("args.length == " + args.length);
 
-        int iterations = 3;
+        int numQuestionsInt = 3;
+        int numCorrectInt = 0;
 
         Scanner userInput1 = new Scanner(System.in);  // Create a Scanner object
         System.out.print("Which tables [1-12]? ");
@@ -19,7 +20,7 @@ public class TimesTables {
             choiceInt = Integer.parseInt(choiceStr);
         }
         catch (NumberFormatException e) {
-            System.out.println("That doesn't seem to be an integer");
+            System.out.println("That doesn't seem to be an integer.");
             System.exit(1);
         }
 
@@ -28,24 +29,34 @@ public class TimesTables {
             System.exit(1);
         }
 
-        // nextInt is normally exclusive of the top value,
-        // so add 1 to make it inclusive
-        int randInt = ThreadLocalRandom.current().nextInt(1, 13);
+        for(int i = 1; i <= numQuestionsInt; i++){
+            // nextInt is normally exclusive of the top value,
+            // so add 1 to make it inclusive
+            int randInt = ThreadLocalRandom.current().nextInt(1, 13);
 
-        for(int i = 1; i <= iterations; i++){
             Scanner userInput2 = new Scanner(System.in); // Create another Scanner object
             System.out.print("Turn " + i + ": what is " + choiceStr + " times " + randInt + "? ");
             String answerStr = userInput2.nextLine(); // Read the user answer
 
-            int answerInt;
+            int answerInt = 0;
             try {
                 answerInt = Integer.parseInt(answerStr);
-                System.out.println("choiceStr randInt answerStr == " + choiceStr + " " + randInt + " " + answerStr);
+                //System.out.println("choiceStr randInt answerStr == " + choiceStr + " " + randInt + " " + answerStr);
             }
             catch (NumberFormatException e) {
                 System.out.println("Since that isn't a number, it's definitely wrong.");
             }
+
+            if( answerInt == choiceInt * randInt ) {
+                System.out.println("Correct!");
+                numCorrectInt++;
+            }
+            else {
+                System.out.println("Wrong");
+            }
         }
+
+        System.out.println("You scored " + numCorrectInt + " out of " + numQuestionsInt + ".");
 
     } // main
 
