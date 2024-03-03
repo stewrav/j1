4c6d99e914e2905e0ff23b4bf6e4f477c38595c2
Stewart Ravenhall <stewrav@gmail.com>
2024-03-03 16:41:58 +0000

    Added timing to TimesTables

--- TimesTables.java
+++ TimesTables.java
@@ -5,10 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class TimesTables {
     public static void main(final String[] args) {
 
-        //System.out.println("args.length == " + args.length);
-
-        int numQuestionsInt = 3;
-        int numCorrectInt = 0;
+        int numQuestionsInt = 3; // How many questions to ask
+        int numCorrectInt = 0; // How many answers are correct so far
 
         Scanner userInput1 = new Scanner(System.in);  // Create a Scanner object
         System.out.print("Which tables [1-12]? ");
@@ -19,7 +17,7 @@ public class TimesTables {
         try {
             choiceInt = Integer.parseInt(choiceStr);
         }
-        catch (NumberFormatException e) {
+        catch (NumberFormatException nfe) {
             System.out.println("That doesn't seem to be an integer.");
             System.exit(1);
         }
@@ -29,6 +27,10 @@ public class TimesTables {
             System.exit(1);
         }
 
+        // Get time of day
+        long time0 = System.currentTimeMillis();
+
+        // Loop through the required number of questions
         for(int i = 1; i <= numQuestionsInt; i++){
             // nextInt is normally exclusive of the top value,
             // so add 1 to make it inclusive
@@ -38,15 +40,16 @@ public class TimesTables {
             System.out.print("Turn " + i + ": what is " + choiceStr + " times " + randInt + "? ");
             String answerStr = userInput2.nextLine(); // Read the user answer
 
+            // Read in the user answer
             int answerInt = 0;
             try {
                 answerInt = Integer.parseInt(answerStr);
-                //System.out.println("choiceStr randInt answerStr == " + choiceStr + " " + randInt + " " + answerStr);
             }
             catch (NumberFormatException e) {
                 System.out.println("Since that isn't a number, it's definitely wrong.");
             }
 
+            // Mark the user answer
             if( answerInt == choiceInt * randInt ) {
                 System.out.println("Correct!");
                 numCorrectInt++;
@@ -54,10 +57,23 @@ public class TimesTables {
             else {
                 System.out.println("Wrong");
             }
-        }
 
-        System.out.println("You scored " + numCorrectInt + " out of " + numQuestionsInt + ".");
+        } // Finished looping through the required number of questions
+
+        // Get time of day again
+        long time1 = System.currentTimeMillis();
+
+        // Calculate how long we took, in seconds
+        long timeLong = (time1 - time0) / 1000;
+
+        // Now convert everything to strings ready for printing
+        String numCorrectStr   = Integer.toString(numCorrectInt);
+        String numQuestionsStr = Integer.toString(numQuestionsInt);
+        String timeStr         = Long.toString(timeLong);
+
+        System.out.println("You scored " + numCorrectStr + " out of "
+            + numQuestionsStr + " in " + timeStr + " seconds.");
 
     } // main
 
-} // TimesTables
+}
